ad90794eaabea146fa3fefb8b5e9fc8bf0b1856a
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-10-04 16:24:00 +1000

    styled component + global style + google fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 function App() {
-    return <div>Hello sliceline</div>;
+    return (
+        <React.Fragment>
+            <GlobalStyle />
+            <h1>wtf</h1>
+            <div>hello world</div>
+        </React.Fragment>
+    );
 }
 
 export default App;
