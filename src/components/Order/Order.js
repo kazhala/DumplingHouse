4f98b606c36a378d6c23da41c795f7aa8a034bc9
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-10-05 23:33:53 +1000

    order + food dialog footer

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ConfirmButton, DialogContent } from '../FoodDialog/FoodDialog';
+import { DialogFooter as OrderFooter } from '../FoodDialog/FoodDialog';
 
 const OrderStyled = styled.div`
     position: fixed;
@@ -10,10 +12,24 @@ const OrderStyled = styled.div`
     height: calc(100% - 48px);
     box-shadow: 4px 0px 5px 4px grey;
     z-index: 10;
+    display: flex;
+    flex-direction: column;
+`;
+
+const OrderContent = styled(DialogContent)`
+    padding: 20px;
+    height: 100%;
 `;
 
 const Order = props => {
-    return <OrderStyled>Your order is empty...</OrderStyled>;
+    return (
+        <OrderStyled>
+            <OrderContent>Your order is looking pretty empty...</OrderContent>
+            <OrderFooter>
+                <ConfirmButton>Checkout</ConfirmButton>
+            </OrderFooter>
+        </OrderStyled>
+    );
 };
 
 export default Order;
